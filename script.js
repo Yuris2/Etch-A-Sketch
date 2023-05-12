acf3f02fd7ecb4dd5f5dac81c89604fbaf04b2a2
Yuri Batan <batanyuri@gmail.com>
2023-05-12 08:06:01 -0500

    Added permanent color changing divs once the div is hovered.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const gridContainer = document.getElementById("grid-container")
 
 for (let i = 0; i < 16*16; i++ ) {
     const grid = document.createElement("div");
-    grid.classList.add("canvas")
+    grid.classList.add("canvas");
+    grid.addEventListener("mouseover", function () {
+        grid.style.backgroundColor = "red";
+    })
     gridContainer.appendChild(grid)
 }
