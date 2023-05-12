552af041fd6439d888ff7748bb95be627e3ab8e2
Yuri Batan <batanyuri@gmail.com>
2023-05-12 07:42:36 -0500

    Organized divs into 16x16 square and aligned the canvas to center-ish of page

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const gridContainer = document.getElementById("grid-container")
 
-for (let i = 0; i < 16; i++ ) {
+for (let i = 0; i < 16*16; i++ ) {
     const grid = document.createElement("div");
-    grid.innerHTML = "Testing"
     grid.classList.add("canvas")
     gridContainer.appendChild(grid)
 }
