feb51c9a753b89a13849ebb73b30652cad0fe368
Yuri Batan <batanyuri@gmail.com>
2023-05-15 08:11:18 -0500

    Placed all of the sizing into a javaScript funcition to add the ability to adjust the size later in the future

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const gridContainer = document.getElementById("grid-container")
+function createBoard (size) {
+    const gridContainer = document.getElementById("grid-container")
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    let sizeOfBoard = size * size;
 
-for (let i = 0; i <16*16; i++ ) {
+    for (let i = 0; i <sizeOfBoard; i++ ) {
     const grid = document.createElement("div");
     grid.classList.add("canvas");
     grid.addEventListener("mouseover", function () {
@@ -9,6 +13,7 @@ for (let i = 0; i <16*16; i++ ) {
     gridContainer.appendChild(grid)
 }
 
+}
 function cleared() {
     const cells = document.querySelectorAll(".canvas");
     cells.forEach(function(cell) {
@@ -16,5 +21,8 @@ function cleared() {
     });
 }
 
+createBoard(16);
+
 //Next Steps
 //adjustable grid EX: shrinks to grows size of grid
+//I guess I have to study CSS Variables in order to make it easier
